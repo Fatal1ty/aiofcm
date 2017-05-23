b8d777cbf991f8a37cbc3c714543a2fd6126d81d
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2017-05-23 16:36:14 +0300

    Bump to 0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiofcm',
-    version='0.4',
+    version='0.5',
     description='An efficient Firebase Cloud Messaging Client Library '
                 'for Python/asyncio',
     long_description=open('README.rst').read(),
